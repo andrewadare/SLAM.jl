585cb2982e07ce519751437a461e556f23167e20
Andrew Adare <andrewadare@gmail.com>
2016-01-21 21:51:32 -0700

    Move observe_model to common.jl

--- common.jl
+++ common.jl
@@ -163,3 +163,40 @@ function augment(x, P, z, R)
     end
     x, P
 end
+
+function observe_model(x::AbstractVector, idf::Int)
+    # INPUTS:
+    #   x - state vector
+    #   idf - index of feature order in state
+    #
+    # OUTPUTS:
+    #   z - predicted observation
+    #   H - observation Jacobian
+    #
+    # Given a feature index (ie, the order of the feature in the state vector),
+    # predict the expected range-bearing observation of this feature and its Jacobian.
+
+    Nxv = 3 # number of vehicle pose states
+    fpos = Nxv + idf*2 - 1 # position of xf in state
+    H = zeros(2, length(x))
+
+    # auxiliary values
+    dx = x[fpos] - x[1] 
+    dy = x[fpos + 1] - x[2]
+    d2 = dx^2 + dy^2
+    d = sqrt(d2)
+    xd = dx/d
+    yd = dy/d
+    xd2 = dx/d2
+    yd2 = dy/d2
+    phi = x[3]
+
+    # predict z
+    z = [d; atan2(dy,dx) - phi]
+
+    # calculate H
+    H[:,1:3]         = [-xd -yd 0; yd2 -xd2 -1]
+    H[:,fpos:fpos+1] = [ xd  yd;  -yd2  xd2]
+ 
+    z, H
+end
