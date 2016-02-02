95fb84b6c0248bf567ae85f6d1e8ea72a4163268
Andrew Adare <andrewadare@gmail.com>
2016-02-01 20:41:46 -0700

    Add initial_waypoints() function

--- common.jl
+++ common.jl
@@ -2,7 +2,7 @@
 
 ## Core Types ##
 
-type Scene{T<:Real}
+immutable Scene{T<:Real}
     boundaries::Vector{T}              # Scene boundaries
     waypoints::Matrix{T}               # Waypoint positions
     landmarks::Matrix{T}               # True (simulated) feature positions
