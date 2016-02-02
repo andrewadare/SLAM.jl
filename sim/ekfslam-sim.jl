f23a618b59805fed0eb43658bb0a6551af8a9e8d
Andrew Adare <andrewadare@gmail.com>
2016-02-02 14:59:47 -0700

    Get rid of global const variables

--- sim/ekfslam-sim.jl
+++ sim/ekfslam-sim.jl
@@ -1,24 +1,18 @@
 # SLAM simulation using extended Kalman filter. Inspired by Matlab implementation
 # by T. Bailey et al (https://openslam.org/bailey-slam.html).
 
-# Get location of SLAM module and make sure it's in LOAD_PATH
-# module_dir = dirname(dirname(@__FILE__))
-slam_dir = "$(pwd())/.."
-slam_dir in LOAD_PATH || push!(LOAD_PATH, slam_dir)
-
 using SLAM
 
 include("sim-utils.jl")
 include("gr-draw.jl")
 
-# Scene boundaries: xmin, xmax, ymin, ymax
-const BOUNDARIES = [0.; 100; 0; 100]
-const N_LANDMARKS = 10::Int
+function ekfsim_setup(n_landmarks::Integer, waypoints_file::AbstractString)
 
-function ekfsim_setup()
-    scene = Scene(BOUNDARIES,
-                  get_waypoints(joinpath(slam_dir, "course1.txt")),
-                  make_landmarks(N_LANDMARKS, BOUNDARIES, 0.05),
+    # Scene boundaries: xmin, xmax, ymin, ymax
+    boundaries = [0.; 100; 0; 100]
+    scene = Scene(boundaries,
+                  get_waypoints(waypoints_file),
+                  make_landmarks(n_landmarks, boundaries, 0.05),
                   Array{Float64}(3, 10000),
                   Array{Float64}(3, 10000),
                   0)
@@ -41,9 +35,10 @@ function ekfsim_setup()
     scene, vehicle, state
 end
 
-function run_ekfsim(scene, vehicle, state)
+function sim(scene::Scene, vehicle::Vehicle, state::EKFSlamState)
+    n_landmarks = size(scene.landmarks, 2)
 
-    lmtags = 1:N_LANDMARKS             # Unique identifier for each landmark
+    lmtags = 1:n_landmarks             # Unique identifier for each landmark
 
     d_min = 1.0                        # [m] Once inside d_min, head for next waypoint
     nlaps = 2                          # Number of loops through the waypoint list
@@ -53,7 +48,6 @@ function run_ekfsim(scene, vehicle, state)
     sigma_speed = 0.5                  # [m/s] Uncertainty about target speed
     sigma_steer = (3.0*pi/180)         # [rad] Uncertainty about target gamma
     Q = [sigma_speed^2 0; 0 sigma_steer^2]
-    QE = Q
 
     ## Observation parameters ##
     sensor_range = 30                  # [m] Landmark detection radius
@@ -61,19 +55,13 @@ function run_ekfsim(scene, vehicle, state)
     sigmaR = 0.1                       # [m] Range uncertainty
     sigmaB = (1.0*pi/180)              # [rad] Bearing angle uncertainty
     R = [sigmaR^2 0; 0 sigmaB^2]       # Obs. covariance matrix
-    RE = R
-
-    ## Data association parameters ##
-    # Innovation thresholds or "gates" (Mahalanobis distances)
-    gate_rej = 4.0                     # [m] max distance for association
-    gate_add = 25.0                    # [m] min distance for creation of new feature
 
     ## Bookkeeping variables ##
     dtsum = 0                          # Time since last observation
-    da_table = zeros(1, N_LANDMARKS)   # Data association table 
+    da_table = zeros(1, n_landmarks)   # Data association table 
     
     # GR.beginprint("racecourse.png") # Bug: outputs gks.png instead
-    init_plot_window(BOUNDARIES)
+    init_plot_window(scene.boundaries)
     # GR.endprint()
 
     draw_map(scene.landmarks, scene.waypoints)
@@ -98,7 +86,7 @@ function run_ekfsim(scene, vehicle, state)
         add_control_noise!(vehicle, Q)
        
         # Prediction update for state vector and covariance
-        state.x, state.cov = predict(state, vehicle, QE, dt)
+        state.x, state.cov = predict(state, vehicle, Q, dt)
 
         dtsum += dt
 
@@ -107,13 +95,16 @@ function run_ekfsim(scene, vehicle, state)
             dtsum = 0
             z, tags = get_observations(vehicle.pose, scene.landmarks, lmtags, sensor_range, R)
 
-            zf, idf, zn = associate(state, z, RE, gate_rej, gate_add)
+            # Last two parameters are thresholds (Mahalanobis distances)
+            # 4.0  [m] max distance for association
+            # 25.0 [m] min distance for creation of new feature
+            zf, idf, zn = associate(state, z, R, 4.0, 25.0)
 
             # Update SLAM state
-            state.x, state.cov = update(state, zf, RE, idf)
+            state.x, state.cov = update(state, zf, R, idf)
 
             # Add z measurements to the SLAM state vector and its covariance
-            state.x, state.cov = add_features(state, zn, RE)
+            state.x, state.cov = add_features(state, zn, R)
         end
 
         # Update vehicle tracks in scene
