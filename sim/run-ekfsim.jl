fa4c3318817ad24ea37cc5eae0ef4c1783cd8cf3
Andrew Adare <andrewadare@gmail.com>
2016-02-02 16:15:00 -0700

    Example driver script for EKF SLAM simulation

---
+++ sim/run-ekfsim.jl
@@ -0,0 +1,13 @@
+
+module_dir = "$(pwd())/.."
+module_dir in LOAD_PATH || push!(LOAD_PATH, module_dir)
+
+include("ekfslam-sim.jl")
+
+function monitor(scene::Scene, vehicle::Vehicle, state::SlamState)
+    println(scene.nsteps)
+end
+
+ekfsim_setup(10, "course1.txt")
+scene, vehicle, state = ekfsim_setup(10, "course1.txt")
+sim(scene, vehicle, state, monitor)
