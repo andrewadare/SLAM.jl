259ffc9561de663b5392edf5cf92cfa9452d6d91
Andrew Adare <andrewadare@gmail.com>
2016-02-04 12:41:40 -0700

    Factor message writing into send_json() and use new state_updated flag to selectively send out observation data

--- sim/browser/wsserver.jl
+++ sim/browser/wsserver.jl
@@ -7,38 +7,77 @@ module_dir in LOAD_PATH || push!(LOAD_PATH, module_dir)
 
 include("../ekfslam-sim.jl")
 
-# Global object containing simulation state, updated in real time. 
+# Global object containing simulation state, updated in real time.
 # It is exposed for monitoring, debugging, and flexible visualization.
 simdata = ekfsim_setup(10, "../course1.txt")
+
+function send_json(name::AbstractString, data::Any, client::WebSockets.WebSocket)
+    msg = Dict{AbstractString, Any}()
+    msg["type"] = name
+    msg["data"] = data
+    msg["timestamp"] = time()
+    write(client, JSON.json(msg))
+end
 
 function monitor(simdata::SimData, client::WebSockets.WebSocket)
 
     n = simdata.scene.nsteps
     tt, st = simdata.scene.true_track, simdata.scene.slam_track
-    msg = Dict{AbstractString, Any}()
+    # msg = Dict{AbstractString, Any}()
 
-    # Write track data
-    msg["type"] = "tracks"
-    msg["data"] = Dict("ideal" => Dict(
-                            "x"   => tt[1, n],
-                            "y"   => tt[2, n],
-                            "phi" => tt[3, n]),
-                       "slam" => Dict(
-                            "x"   => st[1, n],
+    # # Write track data
+    # msg["type"] = "tracks"
+    # msg["data"] = Dict("ideal" => Dict(
+    #                         "x"   => tt[1, n],
+    #                         "y"   => tt[2, n],
+    #                         "phi" => tt[3, n]),
+    #                    "slam" => Dict(
+    #                         "x"   => st[1, n],
+    #                         "y"   => st[2, n],
+    #                         "phi" => st[3, n])
+    #                    )
+    # msg["timestamp"] = time()
+    # write(client, JSON.json(msg))
+
+    # Send latest track history
+    d = Dict("ideal" => Dict("x"   => tt[1, n],
+                             "y"   => tt[2, n],
+                             "phi" => tt[3, n]),
+             "slam" => Dict("x"   => st[1, n],
                             "y"   => st[2, n],
-                            "phi" => st[3, n])
-                       )
-    msg["timestamp"] = time()
-    write(client, JSON.json(msg))
+                            "phi" => st[3, n]))
+    send_json("tracks", d, client)
 
-    # Write EKF SLAM state data (reassigning all msg keys)
-    msg["type"] = "state"
-    msg["data"] = Dict("pose" => simdata.state.x[1:3],
-                       "cov"  => simdata.state.cov,
-                       "z"    => simdata.z,
-                       "nobs" => simdata.nz)
-    msg["timestamp"] = time()
-    write(client, JSON.json(msg))
+    # Send SLAM state
+    d = Dict("pose" => simdata.state.x[1:3], "cov" => simdata.state.cov)
+    send_json("state", d, client)
+
+    # Send observations
+    if simdata.state_updated
+        d = Dict("z" => simdata.z, "nobs" => simdata.nz)
+        send_json("observations", d, client)
+    end
+
+    # # Write EKF SLAM state data (reassigning all msg keys)
+    # msg["type"] = "state"
+    # msg["data"] = Dict("pose" => simdata.state.x[1:3],
+    #                    "cov"  => simdata.state.cov,
+    #                    "z"    => simdata.z,
+    #                    "nobs" => simdata.nz)
+    # msg["timestamp"] = time()
+    # write(client, JSON.json(msg))
+
+    # # Write observation data (reassigning all msg keys)
+    # msg["type"] = "state"
+    # msg["data"] = Dict("pose" => simdata.state.x[1:3],
+    #                    "cov"  => simdata.state.cov,
+    #                    "z"    => simdata.z,
+    #                    "nobs" => simdata.nz)
+    # msg["timestamp"] = time()
+    # write(client, JSON.json(msg))
+
+
+
     return
 end
 
@@ -63,7 +102,7 @@ wsh = WebSocketHandler() do req, client
     println("    client.socket: ",     client.socket)
     println("    client.is_closed: ",  client.is_closed)
     println("    client.sent_close: ", client.sent_close)
-    
+
     while true
 
         # Read string from client, decode, and parse to Dict
@@ -91,7 +130,7 @@ Files will not be loaded by browser unless they appear in this list!
 Paths are relative to the location of this script (or invocation location?)
 """
 httph = HttpHandler() do req::Request, res::Response
-    
+
     # Handle case where / means index.html
     if req.resource == "/"
         println("serving ", req.resource)
